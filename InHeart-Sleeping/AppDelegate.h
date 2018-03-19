72b2d96a0eb96cf926ef3964e8aa27d2c7d0f919
项小盆友 <1259653875@qq.com>
2018-03-19 17:43:32 +0800

    新建项目

---
+++ InHeart-Sleeping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InHeart-Sleeping
+//
+//  Created by 项小盆友 on 2018/3/19.
+//  Copyright © 2018年 项小盆友. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+target 'InHeart-Sleeping' do
+pod 'AFNetworking'
+pod 'YYModel'
+pod 'Masonry'
+pod 'IQKeyboardManager'
+pod 'MBProgressHUD'
+pod 'SDWebImage'
+end 
+
